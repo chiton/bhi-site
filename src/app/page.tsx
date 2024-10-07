3d5ba6eff829db2960088d3f5bf9e95ab514cc62
aanton <aanton@edg.io>
2024-10-07 17:05:08 -0300

    Update image path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import logo from "public/logo.svg";
+import logo from "/public/logo.svg";
 import TestimonialsCarousel from '../components/TestimonialsCarousel';
 
 export default function Home() {
